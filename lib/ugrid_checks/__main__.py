54f100e62542e8c29a75dc6f64a86bec6bccd6e8
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2021-12-31 19:46:04 +0000

    Added cli.

---
+++ lib/ugrid_checks/__main__.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    from ugrid_checks.cli import call_cli
+
+    rc = call_cli()
+    exit(rc)
---
+++ tools/ugrid_checker.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from ugrid_checks.cli import call_cli
+
+rc = call_cli()
+exit(rc)
