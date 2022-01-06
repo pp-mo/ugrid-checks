db7eae80882b19f44cd5139347bb9bba4dd85c2c
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2022-01-06 14:28:11 +0000

    Added structure report and version statements.

--- lib/ugrid_checks/cli.py
+++ lib/ugrid_checks/cli.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 
+import ugrid_checks
 from ugrid_checks.check import check_dataset
 
 
@@ -26,6 +27,21 @@ def make_parser():
             'i.e. suppress warnings ("Axxx"= advise codes).'
         ),
     )
+    parser.add_argument(
+        "-s",
+        "--summary",
+        action="store_true",
+        help="Print a summary of mesh structure found in the file.",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="store_true",
+        help=(
+            "Print versions of ugrid-checker, "
+            "and latest known-compatible CF version."
+        ),
+    )
     return parser
 
 
@@ -33,11 +49,26 @@ def call_cli(args=None):
     parser = make_parser()
     args = parser.parse_args(args)
 
+    if args.version:
+        checker_vsn = ugrid_checks.__version__
+        cf_vsn = ugrid_checks._cf_version
+        print("")
+        print(f"UGRID file checker, version {checker_vsn}")
+        print(f"    valid to CF version : {cf_vsn}")
+
     checker = check_dataset(
         file=args.file,
         print_summary=False,  # print summary separately, if needed
         omit_advisories=args.errorsonly,
     )
+
+    if args.summary:
+        print("")
+        print("File mesh structure")
+        print("-------------------")
+        print(checker.structure_report())
+        print("")
+
     rc = 0
     log = checker.logger
     if log.N_FAILURES > 0:
